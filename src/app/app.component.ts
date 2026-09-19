0c92cf77074551d00d7330aa2c9c63f99e520219
anotherfaizan <mdfaizankhan2002@gmail.com>
2026-09-19 21:22:03 +0530

    task component
    Output Emitter with type binding
    User Name Propagation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,26 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TaskComponent } from './task/task.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  selectedUserId = 'u1';
+
+  get userSelected(){
+    return this.users.find( (user)=> user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string){
+    this.selectedUserId = id;
+  }
 }
