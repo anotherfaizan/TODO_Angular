9b19807b1e5f2f317786cddebbd7a89b918fd8e2
anotherfaizan <mdfaizankhan2002@gmail.com>
2026-09-19 21:40:33 +0530

    interface and type object

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,6 +4,18 @@ import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// }
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -12,16 +24,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true}) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
